b64c1b41d6727d36390840e01a2230d3e9320148
jmueller734 <jmueller734@fhs.win.fuhsd.org>
2016-10-28 09:36:16 -0700

    10/28 9:36

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -1,22 +1,20 @@
 public class StepCounter {
 	public static void main(String[] args) {
 		String[] columnNames={"time", "gyro-x", "gyro-y", "gyro-z"};
-		CSVData test = new CSVData("data/walkingSampleData-out.csv", columnNames, 1);
-		System.out.println(test.getColumn(0).length);
+		CSVData test = new CSVData("data/64StepsInPocketJogging-out.csv", columnNames, 1);
 		test.correctTime(test);
 		System.out.println(countSteps(test.getColumn(0),test.getRows(1,test.getNumRows()-1)));
 	}
 	private static int countSteps(double[] times, double[][] sensorData) {
 		int stepCount = 0;
 		double[] arr = new double[times.length];
-		System.out.println(times.length);
-		System.out.println(arr.length);
 		arr = calculateMagnitudes(sensorData);
 		double mean = calculateMean(arr);
 		double deviation = calculateStandardDeviation(arr, mean);
+		System.out.println(mean+deviation);
 		for(int i = 1; i < arr.length-1; i++) {
 			if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
-				if(arr[i] > 4*deviation) {
+				if(arr[i] > mean+deviation*0.8) {
 					stepCount++;
 					System.out.println(stepCount +" " + times[i]/1000);
 				}
