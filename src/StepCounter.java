fa9e7c1eddc68b54deef7f62d79157024d1f2d6d
jmueller734 <jmueller734@fhs.win.fuhsd.org>
2016-11-04 10:39:31 -0700

    Add data files, implement adaptive thresholding to StepCounter

--- src/StepCounter.java
+++ src/StepCounter.java
@@ -5,37 +5,49 @@ public class StepCounter {
 		String[] columnNames={"time", "gyro-x", "gyro-y", "gyro-z"};
 		CSVData test = new CSVData("data/64StepsInPocketJogging-out.csv", columnNames, 1);
 		test.correctTime(test);
-		System.out.println(countSteps(test.getColumn(0),test.getRows(1,test.getNumRows()-1)));
+		System.out.println(countSteps(test.getColumn(1),test.getRows(1,test.getNumRows()-1), 10));
 	}
-	private static int countSteps(double[] times, double[][] sensorData) {
+	
+	private static int countSteps(double[] times, double[][] sensorData, int windowLength) {
 		int stepCount = 0;
 		double[] arr = new double[times.length];
 		arr = calculateMagnitudes(sensorData);
-		System.out.println(Arrays.toString(arr));
-		arr = NoiseSmoothing.generalRunningAverage(arr, 3);
-		System.out.println(Arrays.toString(arr));
-		double mean = calculateMean(arr);
-		double deviation = calculateStandardDeviation(arr, mean);
-		System.out.println(mean+deviation);
-		for(int i = 1; i < arr.length-1; i++) {
+		//arr = NoiseSmoothing.generalRunningAverage(arr, 3);
+		double[] thresholds = calculateWindow(arr, windowLength);
+		for(int i = 1; i < arr.length; i++) {
 			if (arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
-				if(arr[i] > mean+deviation*0.8) {
+				if(arr[i] > thresholds[i]) {
 					stepCount++;
 					System.out.println(stepCount +" " + times[i]/1000);
 				}
 			}
 		}
-		
-		return stepCount;
-	}
+		return stepCount;	
+	}	
+
+
 	
-/*	public static double[] calculateWindow(double[] arr, int windowLength) {
-		result = array of threshold values, with interval windowLength
-		calculate mean for windowlength
-		calculate deviation for windowlength
-		add mean and deviation together
-		return result
-	}*/
+	public static double[] calculateWindow(double[] arr, int windowLength) {
+		double[] result=new double[arr.length];
+		for (int i = 0; i < arr.length; i++){
+			if(i+windowLength < arr.length) {
+				if(i-windowLength >= 0) {
+					double meanForInterval = calculateMeanInInterval(arr, i-windowLength, i+windowLength);
+					double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, i-windowLength, i+windowLength);
+					result[i] = (meanForInterval+deviationForInterval);
+				} else {
+					double meanForInterval = calculateMeanInInterval(arr, 0, i+windowLength);
+					double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, 0, i+windowLength);
+					result[i] = (meanForInterval+deviationForInterval);
+				}
+			} else {
+				double meanForInterval = calculateMeanInInterval(arr, i-windowLength, arr.length);
+				double deviationForInterval = calculateStandardDeviationInInterval(arr, meanForInterval, i-windowLength, arr.length);
+				result[i] = (meanForInterval+deviationForInterval);
+			}
+		}
+		return result;
+	}
 	
 	public static double[] noiseSmoothing(double[] magnitudes, int averageLength) {
 		double[] result = new double[magnitudes.length-averageLength];
@@ -66,12 +78,28 @@ public class StepCounter {
 		return Math.sqrt(sum/(arr.length-1));
 	}
 	
+	private static double calculateStandardDeviationInInterval(double[] arr, double mean, int startInterval, int endInterval) {
+		double sum = 0;
+		for (int i = startInterval; i < endInterval; i++) {
+			sum += (arr[i] - mean)*(arr[i] - mean);
+		}
+		return Math.sqrt(sum/(double)(endInterval-startInterval));
+	}
+	
 	private static double calculateMean(double[] arr) {
 		double sum = 0;
 		for(int i = 0; i < arr.length; i++) {
 			sum += arr[i];
 		}
 		return sum/arr.length;
+	}
+	
+	private static double calculateMeanInInterval(double[] arr, int startIndex, int endIndex) {
+		double sum = 0;
+		for(int i = startIndex; i < endIndex; i++) {
+			sum += arr[i];
+		}
+		return sum/(double)(endIndex-startIndex);
 	}
 }
 	
